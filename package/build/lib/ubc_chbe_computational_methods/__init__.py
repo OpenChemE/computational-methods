d929a0c0803c70c42dc8bb0c96f390b556480172
armanawn <arman.awn@gmail.com>
2020-09-15 16:57:03 -0700

    minor edits

--- package/build/lib/ubc_chbe_computational_methods/__init__.py
+++ package/build/lib/ubc_chbe_computational_methods/__init__.py
@@ -1,18 +1,19 @@
-import root_finding
-import linear_nonlinear_systems
-import regression
-import numericalintegration
-import ode_solve
+import ubc_chbe_computational_methods.root_finding
+import ubc_chbe_computational_methods.root_finding
+import ubc_chbe_computational_methods.linear_nonlinear_systems
+import ubc_chbe_computational_methods.regression
+import ubc_chbe_computational_methods.numerical_integration
+import ubc_chbe_computational_methods.ode_solve
 
 def load_matplotlib_settings():
     '''
-    This part is for matplotlib configuration
+    This part is for matplotlib configuration.
     '''
     
     import matplotlib as mpl
 
     mpl.rcParams.update({'mathtext.fontset': 'cm'})
-    mpl.rcParams.update({'axes.labelsize': 22})
+    mpl.rcParams.update({'axes.labelsize': 20})
     mpl.rcParams.update({'axes.titlesize': 16})
     mpl.rcParams.update({'axes.linewidth': 0.5})
     mpl.rcParams.update({'axes.labelpad': 12})
