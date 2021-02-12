80167f7d761ce69bfc85029c8285e4336b77eaba
Arman <51729281+armanawn@users.noreply.github.com>
2021-02-11 20:17:57 -0800

    Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(name='ubc_chbe_computational_methods',
       version='0.1.3',
       description='Essential python functions for numerical techniques',
-      long_description='This package contains all functions developed for teaching the course CHBE 230 (Computational Methods). Computational techniques including root-finding, linear and nonlinear systems, curve-fitting, differentiation, integration, and ODE solution methods are implemented. For more information and examples for each method, pelase refer to the github page.',
+      long_description='This package contains all functions developed for teaching the course CHBE 230 (Computational Methods). Computational techniques including root-finding, linear and nonlinear systems, curve-fitting, differentiation, integration, and ODE solution methods are implemented. For more information and examples for each method, please refer to the github page.',
       long_description_content_type='text/markdown',
       url='https://github.com/OpenChemE/Computational_Methods',
       author='Arman Seyed-Ahmadi',
